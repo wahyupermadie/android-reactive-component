9bda1630fb870dd85fdf28d32473b13ecd9792dd
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-05-18 23:40:14 +0700

    refactor(helper): add extention to show and hide view

--- app/src/main/java/com/godohdev/reactivecomponent/external/Helper.kt
+++ app/src/main/java/com/godohdev/reactivecomponent/external/Helper.kt
@@ -1,8 +1,18 @@
 package com.godohdev.reactivecomponent.external
 
+import android.view.View
+
 /**
  *
  * Created by Wahyu Permadi on 14/05/20.
  * Android Engineer
  *
  **/
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
